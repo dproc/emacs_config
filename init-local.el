1ee963ca2561b1e9752594df4a29ce249a3b237a
dproc <dapliu@cisco.com>
2013-08-12 15:45:49 +0800

    geared 2

--- init-local.el
+++ init-local.el
@@ -1,4 +1,9 @@
 (require 'smooth-scrolling)
 (require 'xcscope)
+(defun make-some-files-read-only ()
+  "when file opened is of a certain mode, make it read only"
+  (when (memq major-mode '(c-mode c++-mode java-mode shell-script-mode perl-mode tcl-mode python-mode))
+    (toggle-read-only 1)))
+(add-hook 'find-file-hooks 'make-some-files-read-only)
 
 (provide 'init-local)
