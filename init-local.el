8d8751d1672a531c53985e5f117d0e435d8620ba
dproc <dapliu@cisco.com>
2013-08-13 13:58:17 +0800

    geared 3

--- init-local.el
+++ init-local.el
@@ -1,9 +1,18 @@
 (require 'smooth-scrolling)
 (require 'xcscope)
+
 (defun make-some-files-read-only ()
   "when file opened is of a certain mode, make it read only"
   (when (memq major-mode '(c-mode c++-mode java-mode shell-script-mode perl-mode tcl-mode python-mode))
     (toggle-read-only 1)))
 (add-hook 'find-file-hooks 'make-some-files-read-only)
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
 
 (provide 'init-local)
