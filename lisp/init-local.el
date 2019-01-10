71ebec114aaa5709a3480dfac59ef980b61c2d72
Dapeng <dapengliu.ldp@alibaba-inc.com>
2019-01-10 14:34:52 +0800

    readonly go-mode

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -9,7 +9,7 @@
 ;;;;;;;;;;;;;;;;make file RO;;;;;;;;;;;;;;;;;;
 (defun make-some-files-read-only ()
   "when file opened is of a certain mode, make it read only"
-  (when (memq major-mode '(org-mode c-mode c++-mode java-mode shell-script-mode perl-mode tcl-mode python-mode))
+  (when (memq major-mode '(org-mode c-mode c++-mode java-mode shell-script-mode perl-mode tcl-mode python-mode go-mode))
     (toggle-read-only 1)))
 (add-hook 'find-file-hooks 'make-some-files-read-only)
 ;;;;;;;;;;;;;;;;make file RO end;;;;;;;;;;;;;;;;;;
